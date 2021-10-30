883530c1d09c45a74b659791ceee467f833e79e5
Tugdual Grall <tugdual@gmail.com>
2021-10-30 07:50:38 +0000

    c

--- action.js
+++ action.js
@@ -19,7 +19,7 @@ const action = async () => {
 
 
     if ( state.toLocaleLowerCase() == "delete" ) {
-        await octokit.rest.repos.deleteEnvironment({
+        await  octokit.rest.repos.deleteAnEnvironment({
             owner: github.context.repo.owner,
             repo: github.context.repo.repo,
             environment_id: environment
