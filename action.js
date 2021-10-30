36279ce79ae3aef3c4156dc53d21653fe04ab53a
Tugdual Grall <tugdual@gmail.com>
2021-10-30 06:10:04 +0000

    4

--- action.js
+++ action.js
@@ -4,6 +4,9 @@ const github = require('@actions/github');
 
 const action = async () => {    
     const gitHubToken = core.getInput('github_token');
+    const owner = core.getInput('owner');
+    const repo = core.getInput('repo');
+
     const octokit = github.getOctokit(
                             gitHubToken, 
                             {
@@ -17,11 +20,33 @@ const action = async () => {
 
     console.log("ENV : ", environment);
 
-    await octokit.rest.repos.createDeploymentStatus({
-        ...github.context.repo,
-        deployment_id: environment,
-        state: state,
-    });
+    const deployment = await github.rest.repos.createDeployment({
+        owner: owner,
+        repo: repo,
+        ref: args.gitRef,
+        required_contexts: [],
+        environment: args.environment,
+        auto_merge: false,
+        transient_environment: args.transient,
+      });
+
+    console.log(
+        "deployment",
+
+        JSON.stringify(deployment)
+
+    );
+
+
+    await github.rest.repos.createDeploymentStatus({
+        owner: owner,
+        repo: repo,
+        deployment_id: parseInt(deploymentID, 10),
+        state: "in_progress",
+        auto_inactive: args.autoInactive,
+        log_url: args.logsURL,
+        description: args.description,
+      });
 
 
 
